7b9c1eeb588eb3d6e1ba4acff4934e99c7cfa04c
BarbroNeedToSleep <55791445+BarbroNeedToSleep@users.noreply.github.com>
2025-09-04 14:12:36 +0200

    city class based on the 'city' table in the 'world' database.

--- src/main/java/se/lexicon/model/City.java
+++ src/main/java/se/lexicon/model/City.java
@@ -5,4 +5,74 @@ package se.lexicon.model;
  */
 public class City {
     // TODO: Needs completion
+
+    private int id;
+    private String name;
+    private String countryCode;
+    private String district;
+    private int population;
+
+    public City(int id, String name, String countryCode, String district, int population) {
+        this.id = id;
+        this.name = name;
+        this.countryCode = countryCode;
+        this.district = district;
+        this.population = population;
+    }
+
+    public City(String name, String countryCode, String district, int population){
+        this.name =name;
+        this.countryCode =countryCode;
+        this.district =district;
+        this.population =population;
+
+    }
+
+    // Getters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    // Setters
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return "City:" +
+                " id=" + id + ", name=" + name + ", countryCode=" + countryCode +
+                ", district=" + district + " population =" + population;
+    }
 }
